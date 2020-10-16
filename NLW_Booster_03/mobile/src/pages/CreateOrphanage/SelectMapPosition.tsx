ca8fc0ac804c54cc72ebe10f8a190f2d5b9aad43
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2020-10-16 12:19:19 -0300

    create select map data form

---
+++ NLW_Booster_03/mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Dimensions, Text } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+import MapView, { MapEvent, Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
+import mapMarkerImg from '../../images/map-marker.png';
+
+export default function SelectMapPosition() {
+  const navigation = useNavigation();
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+  const [initialRegion, setInitialRegion] = useState<[number, number]>([0, 0]);
+
+  function handleNextStep() {
+    navigation.navigate('OrphanageData', { position });
+  }
+  function handleSelectMapPosition(event: MapEvent) {
+    setPosition(event.nativeEvent.coordinate);
+  }
+
+  useEffect(() => {
+    async function loadPosition() {
+      const { status } = await Location.requestPermissionsAsync();
+
+      if (status !== 'granted') {
+        alert('Precisamos de sua permição para obter a localização');
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync();
+      const { latitude, longitude } = location.coords;
+
+      setInitialRegion([latitude, longitude]);
+    }
+    loadPosition();
+  }, [initialRegion]);
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        initialRegion={{
+          latitude: initialRegion[0],
+          longitude: initialRegion[1],
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+        style={styles.mapStyle}
+        onPress={handleSelectMapPosition}
+      >
+        {position.latitude !== 0 && (
+          <Marker
+            icon={mapMarkerImg}
+            coordinate={{
+              latitude: position.latitude,
+              longitude: position.longitude,
+            }}
+          />
+        )}
+      </MapView>
+      {position.latitude !== 0 && (
+        <RectButton style={styles.nextButton} onPress={handleNextStep}>
+          <Text style={styles.nextButtonText}>Próximo</Text>
+        </RectButton>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative',
+  },
+
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  nextButton: {
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 56,
+
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 40,
+  },
+
+  nextButtonText: {
+    fontFamily: 'Nunito_800ExtraBold',
+    fontSize: 16,
+    color: '#FFF',
+  },
+});
