70bc2f1e7a76352f8c7418eb871659837ae57cf8
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-04-23 17:24:11 -0300

    feat: NLW 05 node - Aula 05

--- NLW_Booster_05/node/server/src/server.ts
+++ NLW_Booster_05/node/server/src/server.ts
@@ -1,4 +1,5 @@
 import { http } from "./app";
 import './websocket/client'
+import './websocket/admin'
 
 http.listen(3333, () => console.log('Server running in port, 3333'))
--- NLW_Booster_05/node/server/src/services/ConnectionsService.ts
+++ NLW_Booster_05/node/server/src/services/ConnectionsService.ts
@@ -31,4 +31,27 @@ export class ConnectionsService {
 
     return connection
   }
+
+  async findAllWithoutAdmin() {
+    const connections = await this.connectionRepository.find({
+      where: { admin_id: null },
+      relations: ['user']
+    })
+
+    return connections
+  }
+  async findBySocketId(socket_id: string) {
+    const connection = await this.connectionRepository.findOne({
+      socket_id
+    })
+
+    return connection
+  }
+  async updateAdminId(user_id: string, admin_id: string) {
+    await this.connectionRepository.createQueryBuilder().update(Connection)
+      .set({ admin_id })
+      .where("user_id = :user_id", {
+        user_id
+      }).execute()
+  }
 }
