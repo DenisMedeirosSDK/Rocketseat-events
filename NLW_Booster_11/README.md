6eb3f98bf3b14e3e47d01a165b4da8ee79edae94
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2023-02-17 09:49:09 -0300

    NLW 11: habit

---
+++ NLW_Booster_11/README.md
@@ -0,0 +1,13 @@
+# NLW - 11 - HABIT
+
+## 💻 Projeto
+
+Este projeto tem como objetivo ajudar na organização do usuário.
+
+Entre em cada uma das pastas: backend, web, mobile e execute
+
+```bash
+npm install
+```
+
+e de acordo com cada package.json execute o comando para rodar.
---
+++ NLW_Booster_11/mobile/App.tsx
@@ -0,0 +1,23 @@
+import { Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold, useFonts } from '@expo-google-fonts/inter';
+import { StatusBar } from 'expo-status-bar';
+import { Loading } from './src/components/Loading';
+import './src/lib/dayjs';
+import { Routes } from './src/routes';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold
+  })
+
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    )
+  }
+  return (
+    <>
+      <Routes />
+      <StatusBar style="light" backgroundColor='transparent' translucent />
+    </>
+  );
+}
